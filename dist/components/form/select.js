88147777220e6d3cc1810a1922e141ea0d3bf3f8
Miguel Sánchez <miguel@glue.gl>
2015-01-08 11:19:58 +0100

    select.js, select.coffee: Changed structure

--- dist/components/form/select.js
+++ dist/components/form/select.js
@@ -1,18 +1,14 @@
-var React, div, label, select, _ref;
+var React, select;
 
-React = require("react");
+React = require('react');
 
-_ref = React.DOM, div = _ref.div, label = _ref.label, select = _ref.select;
+select = React.DOM.select;
 
 module.exports = React.createClass({
   render: function() {
-    return div({
-      className: "field-set"
-    }, div({
-      className: "field"
-    }, select({
-      className: "select-form",
+    return select({
+      className: 'select-form',
       name: this.props.name
-    }, this.props.options)));
+    }, this.props.options);
   }
 });
--- src/components/form/select.coffee
+++ src/components/form/select.coffee
@@ -1,16 +1,10 @@
-React = require("react")
-{ div, label, select } = React.DOM
+React = require 'react'
+{ select } = React.DOM
 
 module.exports = React.createClass
   render: ->
-    div(
-      className: "field-set"
-      , div(
-        className: "field"
-        , select(
-          className: "select-form"
-          name: @props.name
-          , @props.options
-        )
-      )
+    select(
+      className: 'select-form'
+      name: @props.name
+      , @props.options
     )
