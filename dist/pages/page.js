eb42b4969a6b7b1e7d8f8f23284a6330d99f061b
Miguel Sánchez <miguel@glue.gl>
2015-01-12 08:43:29 +0100

    page.js, page.coffee: Updated for languages

--- dist/pages/page.js
+++ dist/pages/page.js
@@ -1,13 +1,38 @@
-var React, div;
+var LocaleSelect, React, ReactIntl, div, section, _ref;
 
 React = require('react');
 
-div = React.DOM.div;
+ReactIntl = require('react-intl');
+
+LocaleSelect = React.createFactory(require('../components/intl/localeSelect'));
+
+_ref = React.DOM, section = _ref.section, div = _ref.div;
 
 module.exports = React.createClass({
-  render: function() {
-    return div({
-      id: 'container'
+  mixins: [ReactIntl],
+  getInitialState: function() {
+    return {
+      locale: this.props.intl.locale
+    };
+  },
+  updateLocale: function(newLocale) {
+    return this.setState({
+      locale: newLocale
     });
+  },
+  render: function() {
+    return section({
+      id: 'page'
+    }, div({
+      id: 'locale-select'
+    }, LocaleSelect({
+      availableLocales: this.props.intl.availableLocales,
+      value: this.state.locale,
+      onChange: this.updateLocale
+    })), div({
+      id: 'content'
+    }, React.createElement(this.props.element, {
+      messages: this.props.intl.messages[this.state.locale]
+    })));
   }
 });
--- src/pages/page.coffee
+++ src/pages/page.coffee
@@ -1,7 +1,33 @@
 React = require 'react'
+ReactIntl = require 'react-intl'
 
-{div} = React.DOM
+LocaleSelect = React.createFactory require '../components/intl/localeSelect'
+
+{ section, div } = React.DOM
 
 module.exports = React.createClass
+
+  mixins: [ReactIntl]
+
+  getInitialState: ->
+    { locale: @props.intl.locale }
+
+  updateLocale: (newLocale) ->
+    @setState { locale: newLocale }
+
   render: ->
-    return (div { id: 'container' })
+    section(
+      id: 'page'
+      , div(
+        id: 'locale-select'
+        , LocaleSelect
+          availableLocales: @props.intl.availableLocales
+          value: @state.locale
+          onChange: @updateLocale
+      )
+      , div(
+        id: 'content'
+        , React.createElement @props.element,
+          messages: @props.intl.messages[@state.locale]
+      )
+    )
