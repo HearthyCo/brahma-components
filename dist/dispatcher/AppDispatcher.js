7885a1dd1e721568a0bb737bd3ea46acb2eb4e6c
Ignacio Lago <ignacio@glue.gl>
2015-01-14 16:20:09 +0100

    dist/ ignored

--- dist/dispatcher/AppDispatcher.js
+++
@@ -1,9 +0,0 @@
-var AppDispatcher, Backbone, _;
-
-_ = require('underscore');
-
-Backbone = require('exoskeleton');
-
-AppDispatcher = _.extend({}, Backbone.Events);
-
-module.exports = AppDispatcher;
